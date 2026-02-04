446e3f350cefd4b558b6c3867ff4e7f1333c0d72
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-02-04 10:00:29 +0530

    first commit

---
+++ identifier.js
@@ -0,0 +1,21 @@
+// Rules for JavaScript Identifiers
+
+// An identifier must follow these rules:
+
+// Can contain letters, digits, underscores (_), and dollar signs ($)
+
+// Must start with a letter, _, or $ (not a digit)
+
+// Case-sensitive (total ≠ Total)
+
+// Cannot be a reserved keyword (like let, if, class)
+
+// No spaces allowed
+
+
+let age;
+let userName;
+let _total;
+let $price;
+let user1;
+
---
+++ variable.js
@@ -0,0 +1,11 @@
+// variables are declared automatically
+x = 10;
+console.log(x);
+
+// var keyword
+var x = 10;
+
+// ES6 - 2015
+let x = 20;
+
+const x = 30;
