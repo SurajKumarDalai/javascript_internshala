4903ef151b2fab2ce298590e578bc4187721a289
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-02-12 14:27:54 +0530

    operators file

---
+++ operators.js
@@ -0,0 +1,36 @@
+// Type of operators
+// 1. Arithmetic operators
+// 2. Comparison operators
+// 3. Logocal operators
+// 4. Assignment operators
+// 5. Conditional operators
+// 6. type operators
+
+// Arithmetic operators
+
+let a =10;
+let b = 20;
+
+let x = a + b;  //Addition
+console.log(x);
+
+console.log(a - b);   //Subtraction
+
+
+console.log(a * b);  //Multiplication
+
+console.log(a / b);  //Division
+
+console.log(a % b);  //Modulus
+
+console.log(a ** 3);   //Exponentiation
+
+let y = 5;
+y++;
+
+console.log(y);  //Increment
+
+let c = 5;
+c--;
+
+console.log(c); //Decrement
