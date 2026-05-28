3040e615a6cb044bc956ffe8b871f106d4864b12
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-05-28 22:17:12 +0530

    linear search example

--- DSA/linear-search.js
+++ DSA/linear-search.js
@@ -13,3 +13,52 @@ let linearSearch = function(numbers, target){
     return -1;
 }
 console.log(linearSearch(numbers,30));
+
+// Count Occurrences of Target
+
+let num = [1,2,3,2,4,2,5];
+
+let linearCount = function(number, target){
+
+    let count = 0;
+
+    for(let i=0; i<num.length; i++){
+
+        if(num[i] === target){
+
+            count++;
+        }
+    }
+    return count;
+}
+console.log(linearCount(num,2));
+
+// Find Smallest ODD Number
+// [8, 3, 11, 2, 5, 7]
+// Return: smallest ODD number
+// Output: 3
+
+// Create function:smallestOdd()
+// Odd condition: number % 2 !== 0
+// let minOdd = Infinity;
+
+let oddnum = [8, 3, 11, 2, 5, 7];
+
+let smallestOdd = function(number, target){
+
+    let minOdd = Infinity;
+
+    for(let i=0; i<oddnum.length; i++){
+
+        if(oddnum[i] % 2 !== 0){
+
+            if(oddnum[i] < minOdd){
+
+                minOdd = oddnum[i]
+            }
+        }
+    
+    }
+    return minOdd;
+}
+console.log(smallestOdd(oddnum));
