18197e538344c648233ab2b5200dde4212eb05ef
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-05-29 18:50:53 +0530

    DSA: implement insertion sort algorithm in JavaScript

--- DSA/insertion-sort.js
+++ DSA/insertion-sort.js
@@ -31,3 +31,25 @@ let insertionSort = function(arr){
 }
 
 console.log(insertionSort([5,3,4,1]));
+
+// example 2
+
+let inSort = function(nums){
+
+    for (let i = 1; i < nums.length; i++){
+
+        let current = nums[i];
+
+        let j = i -1;
+
+        while(j >= 0 && nums[j]> current){
+
+            nums[j+1] = nums[j];
+
+            j--;
+        }
+        nums[j+1] = current;
+    }
+    return nums;
+}
+console.log(inSort([8,5,9,4,1]));
