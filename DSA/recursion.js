6f6944be78ae8a9dba58d9aabc0ccfd464a28434
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-06-04 12:37:25 +0530

    example- Print numbers from 1 to n without using loops

--- DSA/recursion.js
+++ DSA/recursion.js
@@ -19,3 +19,48 @@ function factorialNum(m){
         return m * factorialNum(m -1);
 }
 console.log(factorialNum(5));
+
+// Print numbers from 1 to n without using loops
+function printNumbers(n) {
+    if (n === 0) return;
+
+    printNumbers(n - 1);
+
+    console.log(n);
+}
+
+printNumbers(5);
+
+// example
+function test(n) {
+    if (n === 0) return;
+
+    console.log("A", n);
+
+    test(n - 1);
+
+    console.log("B", n);
+}
+
+test(3);
+
+// test(3)
+// │
+// ├─ A 3
+// │
+// ├─ test(2)
+// │   │
+// │   ├─ A 2
+// │   │
+// │   ├─ test(1)
+// │   │   │
+// │   │   ├─ A 1
+// │   │   │
+// │   │   ├─ test(0)
+// │   │   │   └─ return
+// │   │   │
+// │   │   └─ B 1
+// │   │
+// │   └─ B 2
+// │
+// └─ B 3
