9d9fe0c7c6f3cf756b1816957dd1ebd2753e05db
SurajKumarDalai <surajkumardalaiwork@gmail.com>
2026-06-09 16:56:06 +0530

    insertion append

--- DSA/insertion-append.js
+++ DSA/insertion-append.js
@@ -1,23 +1,52 @@
-class Node{
-    constructor(data){
+class Node {
+    constructor(data) {
         this.data = data;
         this.next = null;
     }
 }
-class LinkedList{
-        constructor(){
-            this.head = null;
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+
+        let newNode = new Node(data);
+
+        // If list is empty
+        if (this.head === null) {
+            this.head = newNode;
+            return;
         }
 
-        append(data){
+        // Traverse to last node
+        let current = this.head;
 
-            let newNode = new Node(data);
-            let current = this.head;
-
-            while(current.next !== null){
-                current = current.next;
-            }
-            current.next = newNode;
+        while (current.next !== null) {
+            current = current.next;
         }
+
+        // Attach new node
+        current.next = newNode;
+    }
+
+    print() {
+        let current = this.head;
+
+        while (current !== null) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
 
+// Example
+let list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+
+list.print();
